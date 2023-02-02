077ca916970472d0992cd3ea0eb24c30d09eb728
Adelowo Ajibola <adelowo.ajibola@suretree.com>
2023-02-02 01:05:32 +0100

    Fixed typo

--- pages/scaleup003/solicitation-final/index.tsx
+++ pages/scaleup003/solicitation-final/index.tsx
@@ -36,7 +36,7 @@ function Third() {
                 you must download and open the form using Adobe Reader DC or
                 higher. Any other software may result the form to malfunction
                 and/or erase the digital signatures. <br />
-                <br />· Financial Bid Form (attached to the solicitation
+                <br />· Financial Bid Form (attached to the solicitation)
                 notice/email
             </Text>
             {/* <TableContainer>
